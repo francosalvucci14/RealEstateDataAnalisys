e6b2e84c5beb209d53929fa5584ef8f09d86f534
francosalvucci14 <francosalvucci14@gmail.com>
2023-04-11 15:09:44 +0200

    Add terminal argument to scraper

--- web-s.py
+++ web-s.py
@@ -4,13 +4,57 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import sys
 import getopt
+import math
 # Indirizzo sito web
+def FindNumber(string):
+    empty_string = ""
+    for m in string:
+        if m.isdigit():
+            empty_string = empty_string+m
+    return int(empty_string)
 
-def search():
+def SearchNumberOfElements(type,zone):
+    url = f"https://www.immobiliare.it/vendita-{type}/roma/{zone}/?criterio=rilevanza&noAste=1"
+    response = requests.get(url)
+        # Analizzare documento HTML del codice sorgente con BeautifulSoup
+    html = BeautifulSoup(response.text, 'html.parser')
+    number_of_el = html.find('div', class_="in-searchList__title")
+    value = number_of_el.text
+    split = value.split()
+    number = split[0]
+    return int(number)
+
+def search(argv):
+    # Import argument from terminal
+    arg_zone = ""
+    arg_type = ""
+    arg_help = "Help: -z <zone> -t <type>".format(argv[0])
+    
+    try:
+        opts, args = getopt.getopt(argv[1:], "hz:t:", ["help", "zone=", 
+        "type="])
+    except:
+        print(arg_help)
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(arg_help)  # print the help message
+            sys.exit(2)
+        elif opt in ("-z", "--zone"):
+            arg_zone = arg
+        elif opt in ("-t", "--type"):
+            arg_type = arg
+
+    if arg_type == "all":
+        arg_type = "case"
     locali_tot = []
     price_tot = []
-    for i in range(1, 10):
-        url = f"https://www.immobiliare.it/vendita-case/roma/centocelle/?criterio=rilevanza&pag={i}&noAste=1"
+    cycle = SearchNumberOfElements(arg_type,arg_zone)
+    print(cycle)
+    pagine = math.ceil(cycle/25)
+    if pagine  == 1:
+        url = f"https://www.immobiliare.it/vendita-{arg_type}/roma/{arg_zone}/?criterio=rilevanza&noAste=1"
         print(url)
         # Eseguire richiesta GET
         response = requests.get(url)
@@ -28,21 +72,37 @@ def search():
         prices = list()
         for price in price_html:
             prices.append(price.text)
-        # Per eseguire il test, combinare e visualizzare le voci di entrambi gli elenchi
-        # for t in zip(locali,prices):
-        #    print(t)
-        # Salvare le citazioni e gli autori in un file CSV nella directory corrente
-        # Aprire il file con Excel / LibreOffice, ecc.
-        # with open('./citazioni.csv', 'w') as csv_file:
-        #     csv_writer = csv.writer(csv_file, dialect='excel')
-        #     csv_writer.writerows(zip(quotes, authors))
+
         locali_tot += locali
         price_tot += prices
+    else:
+        for i in range(1, pagine):
+            url = f"https://www.immobiliare.it/vendita-{arg_type}/roma/{arg_zone}/?criterio=rilevanza&pag={i}&noAste=1"
+            print(url)
+            # Eseguire richiesta GET
+            response = requests.get(url)
+            # Analizzare documento HTML del codice sorgente con BeautifulSoup
+            html = BeautifulSoup(response.text, 'html.parser')
+            # Estrarre tutte le citazioni e gli autori dal documento HTML
+            locali_html = html.find_all('a', class_="in-card__title")
+            price_html = html.find_all(
+                'li', class_="nd-list__item in-feat__item in-feat__item--main in-realEstateListCard__features--main")
+            # Raccogliere le citazioni in un elenco
+            locali = list()
+            for locale in locali_html:
+                locali.append(locale.text)
+            # Raccogliere gli autori in un elenco
+            prices = list()
+            for price in price_html:
+                prices.append(price.text)
+
+            locali_tot += locali
+            price_tot += prices
 
     return locali_tot, price_tot
 
 
-locali, prezzi = search()
+locali, prezzi = search(sys.argv)
 somma_prezzi, count_locali, media_prezzo = 0, 0, 0
 # for i in range(len(prezzi)):
 #     prezzi[i] = prezzi[i].replace("€","")
