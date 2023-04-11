a8fb1d88b5c9b17c63539604a025d317fa143024
francosalvucci14 <francosalvucci14@gmail.com>
2023-04-11 14:16:50 +0200

    Update Scraper

--- web-s.py
+++ web-s.py
@@ -1,28 +1,3 @@
-# from selenium import webdriver
-# from bs4 import BeautifulSoup
-# import pandas as pd
-
-# driver = webdriver.Firefox()
-# products=[] #List to store name of the product
-# prices=[] #List to store price of the product
-# ratings=[] #List to store rating of the product
-# driver.get("https://francosalvucci14.github.io/bankapp/")
-
-# content = driver.page_source
-# soup = BeautifulSoup(content,"html.parser")
-# for a in soup.findAll('div', attrs={'class':'container'}):
-#     print("ciao")
-#     name=a.find('div', attrs={'class':'author'})
-#     print(name)
-#     comment=a.find('div', attrs={'class':'comment'})
-#     #rating=a.find('div', attrs={'class':'_3LWZlK'})
-#     products.append(name.text)
-#     prices.append(comment.text)
-#     #ratings.append(rating.text) 
-# print(products,prices,ratings)
-# df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
-# df.to_csv('products.csv', index=False, encoding='utf-8')
-
 # Importare moduli
 import requests
 import pandas as pd
@@ -30,11 +5,12 @@ from bs4 import BeautifulSoup
 import sys
 import getopt
 # Indirizzo sito web
+All = "case"
 
 def search():
     locali_tot = []
     price_tot = []
-    for i in range(1,10):
+    for i in range(1, 10):
         url = f"https://www.immobiliare.it/vendita-case/roma/centocelle/?criterio=rilevanza&pag={i}&noAste=1"
         print(url)
         # Eseguire richiesta GET
@@ -43,7 +19,8 @@ def search():
         html = BeautifulSoup(response.text, 'html.parser')
         # Estrarre tutte le citazioni e gli autori dal documento HTML
         locali_html = html.find_all('a', class_="in-card__title")
-        price_html = html.find_all('li', class_="nd-list__item in-feat__item in-feat__item--main in-realEstateListCard__features--main")
+        price_html = html.find_all(
+            'li', class_="nd-list__item in-feat__item in-feat__item--main in-realEstateListCard__features--main")
         # Raccogliere le citazioni in un elenco
         locali = list()
         for locale in locali_html:
@@ -51,28 +28,30 @@ def search():
         # Raccogliere gli autori in un elenco
         prices = list()
         for price in price_html:
-            prices.append(price.text) 
+            prices.append(price.text)
         # Per eseguire il test, combinare e visualizzare le voci di entrambi gli elenchi
-        #for t in zip(locali,prices):
+        # for t in zip(locali,prices):
         #    print(t)
         # Salvare le citazioni e gli autori in un file CSV nella directory corrente
         # Aprire il file con Excel / LibreOffice, ecc.
         # with open('./citazioni.csv', 'w') as csv_file:
         #     csv_writer = csv.writer(csv_file, dialect='excel')
         #     csv_writer.writerows(zip(quotes, authors))
-        locali_tot+=locali
-        price_tot+=prices
-    
-    
-    return locali_tot,price_tot
-locali,prezzi = search()
-somma_prezzi,count_locali,media_prezzo = 0,0,0
+        locali_tot += locali
+        price_tot += prices
+
+    return locali_tot, price_tot
+
+
+locali, prezzi = search()
+somma_prezzi, count_locali, media_prezzo = 0, 0, 0
 # for i in range(len(prezzi)):
 #     prezzi[i] = prezzi[i].replace("€","")
 #     print(prezzi[i])
 #     somma_prezzi+=float(prezzi[i])
 #     count_locali+=1
 # print(somma_prezzi)
-#media_prezzo = somma_prezzi/count_locali
-df = pd.DataFrame({'Locali':locali,'Prezzi':prezzi,'Media prezzo:':media_prezzo})     
+# media_prezzo = somma_prezzi/count_locali
+df = pd.DataFrame({'Locali': locali, 'Prezzi': prezzi,
+                  'Media prezzo:': media_prezzo})
 df.to_csv('Immobiliare.csv', index=False, encoding='utf-8')
